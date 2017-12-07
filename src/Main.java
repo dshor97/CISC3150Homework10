85e728b7abba913605bc6370028f919005cd7440
Dayton Shor <dayton.shor@gmain.com>
2017-12-07 13:47:26 -0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-public class Main extends myPlugin{
-
-    public void whoSaysHello() {
-        System.out.println("Main says hello");
-    }
-
-    public void whoSaysBye() {
-        System.out.println("Main says bye.");
-    }
+public class Main{
+    
 }
