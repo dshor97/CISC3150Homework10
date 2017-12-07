8557a1ef7806782a72cafdcf980751fce23b570f
Dayton Shor <dayton.shor@gmain.com>
2017-12-07 13:39:24 -0500

    Update

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main extends myPlugin{
+
+    public void whoSaysHello() {
+        System.out.println("Main says hello");
+    }
+
+    public void whoSaysBye() {
+        System.out.println("Main says bye.");
+    }
+}
---
+++ src/myPlugin.java
@@ -0,0 +1,4 @@
+public abstract class myPlugin {
+    public void whoSaysHello(){}
+    public void whoSaysBye(){}
+}
