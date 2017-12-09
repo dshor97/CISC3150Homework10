758e46f08b411bf0e06dbff3ce7f72f7082a9edb
Dayton Shor <dayton.shor@gmain.com>
2017-12-09 18:05:36 -0500

    Update

--- src/myPlugin.java
+++ src/myPlugin.java
@@ -1,4 +1,4 @@
 public abstract class myPlugin {
     public void whoSaysHello(){}
     public void whoSaysBye(){}
-}class plugin1 extends myPlugin{    public void whoSaysHello(){        System.out.println("plugin1 says hello.");    }    public void whoSaysBye(){ System.out.println("plugin1 says bye."); }}
+}
